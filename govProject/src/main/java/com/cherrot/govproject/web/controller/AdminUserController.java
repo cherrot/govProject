3d14e5a96a27c1f9876c20e7ef7c9c45f0f23425
Cherrot Luo <cherrot+github@cherrot.com>
2012-06-12 15:37:22 +0800

    Add new methods in service interfaces

--- govProject/src/main/java/com/cherrot/govproject/web/controller/AdminUserController.java
+++ govProject/src/main/java/com/cherrot/govproject/web/controller/AdminUserController.java
@@ -4,10 +4,23 @@
  */
 package com.cherrot.govproject.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author Cherrot Luo<cherrot+dev@cherrot.com>
  */
+@Controller
+@RequestMapping("/admin/user")
 public class AdminUserController {
 
+    @RequestMapping
+    public String viewUser() {
+        return null;
+    }
+
+    public String viewUserList() {
+        return null;
+    }
 }
